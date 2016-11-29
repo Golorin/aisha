7c5dc487c9eaedb6e0fc4a9c1470efdab1030a9a
Omnia Joehar <omniajoehar@Omnias-MacBook-Pro-2.local>
2016-11-29 03:33:59 -0500

    Remove database querying from index router get method so that site can run without server timing out

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var db = req.db;
-  var collection = db.get('bioData');
-  collection.find({}, {}, function(e, docs) {
-    res.render('index', {
-      'bioData'  : docs,
-      'schools' : JSON.stringify(docs.schools)
-    });
-  })
+  res.render('index');
 });
 
 module.exports = router;
